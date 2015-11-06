199401d9e256941b2580e7db01b99b3b4be617ae
Melissa Nemeth <melissanemeth1@gmail.com>
2015-11-05 20:27:51 -0500

    Chapter 5 exercises

---
+++ c05/js/child.js
@@ -0,0 +1,6 @@
+var startItem = document.getElementsByTagName('ul')[0];
+var firstItem = startItem.firstChild;
+var lastItem = startItem.lastChild;
+
+firstItem.setAttribute('class', 'complete');
+lastItem.setAttribute('class', 'cool');
