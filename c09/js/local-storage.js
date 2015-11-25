3cc02e2c4ff2dc121b82f58a0fafb34108f28e0f
Melissa Nemeth <melissanemeth1@gmail.com>
2015-11-25 17:23:05 -0500

    Chapter 9 exercises

---
+++ c09/js/local-storage.js
@@ -0,0 +1,20 @@
+// check if the browser supports the a API
+if (window.localstorage) {
+  
+  // get the form elements
+  var username = document.getElementById('username');
+  var answer = document.getElementById('answer');
+
+  // check to see if the objects have values
+  username.value = localStorage.getItem('username');
+  answer.value = localStorage.getItem('answer');
+
+  // save the data
+  username.addEventListener('input' function() {
+    localStorage.setItem('username', username.value);
+  }, false);
+
+  answer.addEventListener('input', function() {
+    localStorage.setItem('answer', answer.value);
+  }, false);
+}
